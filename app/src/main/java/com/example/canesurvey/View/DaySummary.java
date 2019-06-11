a06d94d5269517374fab07db1d72312ca7aab508
pradeepprogramming <pradeep.programming@gmail.com>
2019-06-11 16:50:30 +0530

    first init

--- app/src/main/java/com/example/canesurvey/View/DaySummary.java
+++ app/src/main/java/com/example/canesurvey/View/DaySummary.java
@@ -1,20 +1,36 @@
 package com.example.canesurvey.View;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.canesurvey.Comman.CommanData;
 import com.example.canesurvey.R;
+import com.example.canesurvey.adapter.DaySummaryAdapter;
+import com.example.canesurvey.model.CompleteSurveyModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DaySummary extends Fragment {
     private View view;
-    public String TAG="DAY SUMMARY";
+    public String TAG = "DAY SUMMARY";
+    List<CompleteSurveyModel> surveyModels = new ArrayList<>();
+    RecyclerView recyclerView;
+    Activity mActivity;
+    TextView tvNoRecord;
 
     public DaySummary() {
         // Required empty public constructor
@@ -25,15 +41,28 @@ public class DaySummary extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =inflater.inflate(R.layout.fragment_day_summary, container, false);
+        view = inflater.inflate(R.layout.fragment_day_summary, container, false);
+        mActivity =getActivity();
         initUI(view);
         return view;
     }
 
     private void initUI(View view) {
+        tvNoRecord =view.findViewById(R.id.tvNoRecord);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        surveyModels = CommanData.conn.survey.getCompleteSurvey();
 
-
-
+        Log.d(TAG, "initUI: "+surveyModels.size());
+        if(surveyModels.size()>0) {
+            tvNoRecord.setVisibility(View.GONE);
+            DaySummaryAdapter mAdapter = new DaySummaryAdapter(mActivity, surveyModels);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(mActivity);
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(mAdapter);
+        }else {
+            tvNoRecord.setVisibility(View.VISIBLE);
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/canesurvey/adapter/DaySummaryAdapter.java
@@ -0,0 +1,77 @@
+package com.example.canesurvey.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.canesurvey.R;
+import com.example.canesurvey.model.CompleteSurveyModel;
+
+import java.util.List;
+
+public class DaySummaryAdapter extends RecyclerView.Adapter<DaySummaryAdapter.MyHolder> {
+
+    Context mContext;
+    List<CompleteSurveyModel> arrayList;
+
+    public DaySummaryAdapter(Context mContext, List<CompleteSurveyModel> arrayList) {
+        this.mContext = mContext;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.day_summary_item, parent, false);
+        return new DaySummaryAdapter.MyHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        holder.tvGrowerName.setText(String.valueOf(arrayList.get(position).getGrow()));
+        holder.tvVarty.setText(String.valueOf(arrayList.get(position).getVariety()));
+        holder.tvMobileNumber.setText(String.valueOf(arrayList.get(position).getMobile()));
+
+
+
+        Log.d(">>>", "onBindViewHolder: ");
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        TextView tvGrowerName, tvVarty, tvCaneType, tvGrowerVill, tvGrowerCode, tvGrowerVillCode,
+                tvMobileNumber, tvCaneArea, tvPlotNum, tvType, tvDataFrom, tvCorner1, tvCorner2, tvCorner3, tvCorner4;
+
+        public MyHolder(@NonNull View view) {
+            super(view);
+            tvGrowerName = view.findViewById(R.id.tvGrowerName);
+            tvVarty = view.findViewById(R.id.tvVarty);
+            tvCaneType = view.findViewById(R.id.tvCaneType);
+
+            tvGrowerCode = view.findViewById(R.id.tvGrowerCode);
+            tvGrowerVill = view.findViewById(R.id.tvGrowerVill);
+            tvGrowerVillCode = view.findViewById(R.id.tvGrowerVillCode);
+            tvMobileNumber = view.findViewById(R.id.tvMobileNumber);
+            tvCaneArea = view.findViewById(R.id.tvCaneArea);
+            tvPlotNum = view.findViewById(R.id.tvPlotNum);
+            tvType = view.findViewById(R.id.tvType);
+            tvDataFrom = view.findViewById(R.id.tvDataFrom);
+            tvCorner1 = view.findViewById(R.id.tvCorner1);
+            tvCorner2 = view.findViewById(R.id.tvCorner2);
+            tvCorner3 = view.findViewById(R.id.tvCorner3);
+            tvCorner4 = view.findViewById(R.id.tvCorner4);
+        }
+    }
+}
