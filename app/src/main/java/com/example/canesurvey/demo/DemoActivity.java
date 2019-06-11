32eef16d2ebfae9f64553a02c5c4ae038c727327
pradeepprogramming <pradeep.programming@gmail.com>
2019-06-11 11:16:25 +0530

    first init

---
+++ app/src/main/java/com/example/canesurvey/demo/DemoActivity.java
@@ -0,0 +1,16 @@
+package com.example.canesurvey.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.canesurvey.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
